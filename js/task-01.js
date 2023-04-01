f626d0c560a76b46a7d73e1e945d11cfecd850fb
Denys Cherednychenko <cherydens@ukr.net>
2023-04-01 16:09:21 +0300

    Complete Task-02

--- js/task-01.js
+++ js/task-01.js
@@ -1,25 +1,3 @@
-// Задание 1
-// В HTML есть список категорий ul#categories.
-
-// Напиши скрипт который:
-
-// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
-// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
-// Для выполнения этой задачи нужно использовать метод forEach() и свойства навигации по DOM.
-
-// В результате, в консоли будут выведены такие сообщения.
-
-// Number of categories: 3
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
-
 const categoriesEl = document.querySelectorAll('ul#categories li.item');
 
 console.log(`Number of categories: ${categoriesEl.length}`);
--- js/task-02.js
+++ js/task-02.js
@@ -1,15 +1,17 @@
-// Задание 2
-// В HTML есть пустой список ul#ingredients.
-
-// <ul id="ingredients"></ul>
-
-// В JavaScript есть массив строк.
-
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 
-// Напиши скрипт, который для каждого элемента массива ingredients:
+const ingredientsContainerEl = document.querySelector('ul#ingredients');
 
-// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
-// Добавит название ингредиента как его текстовое содержимое.
-// Добавит элементу класс item.
-// После чего вставит все <li> за одну операцию в список ul#ingredients.
+const makeIngredients = ingredientsArr => {
+  return ingredientsArr.map(ingredient => {
+    const ingredientEl = document.createElement('li');
+    ingredientEl.textContent = ingredient;
+    ingredientEl.classList.add('item');
+
+    return ingredientEl;
+  });
+};
+
+const ingredientsEl = makeIngredients(ingredients);
+
+ingredientsContainerEl.append(...ingredientsEl);
