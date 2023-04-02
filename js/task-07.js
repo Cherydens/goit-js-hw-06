d8eb76f36feaa9c3186568243d00cbd5f1c7e986
Denys Cherednychenko <cherydens@ukr.net>
2023-04-02 20:01:41 +0300

    complete task-07

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+const fontSizeControlEl = document.querySelector('input#font-size-control');
+
+const textEl = document.querySelector('span#text');
+
+fontSizeControlEl.addEventListener('input', event => {
+  textEl.style.fontSize = `${event.currentTarget.value}px`;
+});
