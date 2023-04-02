89b01cf78303425dc1902d703be419b3086e64e9
Denys Cherednychenko <cherydens@ukr.net>
2023-04-02 20:34:22 +0300

    complete task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+
+const btnChangeColorEl = document.querySelector('button.change-color');
+
+const textBgColorEl = document.querySelector('span.color');
+
+btnChangeColorEl.addEventListener('click', () => {
+  const bcgColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = bcgColor;
+  textBgColorEl.textContent = bcgColor;
+});
