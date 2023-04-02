a3c2f2d4ec0db9e9d8c3aecd028d82a41b28ed03
Denys Cherednychenko <cherydens@ukr.net>
2023-04-02 20:24:22 +0300

    complete task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginFormEl = document.querySelector('form.login-form');
 
+const handleSubmit = event => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Всі поля повинні бути заповнені!');
+  }
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(data);
+  event.currentTarget.reset();
+};
+
+loginFormEl.addEventListener('submit', handleSubmit);
